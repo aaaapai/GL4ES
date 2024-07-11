0f7848806188d8eb85b6188417215bddddae7f20
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-07-11 14:46:13 +0800

    Update init.c

--- src/gl/init.c
+++ src/gl/init.c
@@ -205,7 +205,7 @@ void initialize_gl4es() {
         break;
     }
 
-    SHUT_LOGD("Using GLES %s backend\n", (globals4es.es==1)?"1.1":"2.0");
+    SHUT_LOGD("Using GLES %s backend\n", (globals4es.es==1)?"1.1":"3.2");
 
     env(LIBGL_NODEPTHTEX, globals4es.nodepthtex, "Disable usage of Depth Textures");
 
