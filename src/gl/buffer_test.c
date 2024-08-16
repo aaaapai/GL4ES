04fd6b598414464ddf290e53a68aac7eb146938a
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-16 18:40:11 +0800

    Create buffer_test.c

---
+++ src/gl/buffer_test.c
@@ -0,0 +1,13 @@
+#include "const.h"
+void gl4es_glBindBuffer();
+void gl4es_glCopyBufferSubData();
+
+void gl4es_buffer_copy(GLenum target, GLuint buffer, GLuint buffer, GLintptr writeOffset, GLintptr readOffset, GLsizeiptr size)
+{
+   gl4es_glBindBuffer(GL_COPY_READ_BUFFER, GLuint buffer);
+   gl4es_glBindBuffer(GL_COPY_WRITE_BUFFER, GLuint buffer);
+
+   gl4es_glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, GLintptr readOffset, GLintptr writeOffset, GLsizeiptr size);
+   gl4es_glBindBuffer(GL_COPY_READ_BUFFER, 0);
+   gl4es_glBindBuffer(GL_COPY_WRITE_BUFFER, 0);
+};
