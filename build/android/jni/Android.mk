5698e255a55d9831b9f3ae09b83cbda091b4866e
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-23 18:00:07 +0800

    Update Android.mk

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -3,10 +3,10 @@ $(info $(LOCAL_PATH))
 include $(CLEAR_VARS)  
 LOCAL_LDLIBS := -ldl -lm -lc
 LOCAL_MODULE    := glshim
-GL_SRC_FILES := $(wildcard $(LOCAL_PATH)/proxy/*.c) $(wildcard $(LOCAL_PATH)/glx/*.c) $(wildcard $(LOCAL_PATH)/gl/*.c) $(wildcard $(LOCAL_PATH)/gl/wrap/*.c) $(wildcard $(LOCAL_PATH)/gl/math/*.c)
+GL_SRC_FILES := $(wildcard $(LOCAL_PATH)/proxy/host/*.c) $(wildcard $(LOCAL_PATH)/proxy/client/src/*.c) $(wildcard $(LOCAL_PATH)/glx/*.c) $(wildcard $(LOCAL_PATH)/gl/*.c) $(wildcard $(LOCAL_PATH)/gl/wrap/*.c) $(wildcard $(LOCAL_PATH)/gl/math/*.c)
 LOCAL_SRC_FILES := $(GL_SRC_FILES:$(LOCAL_PATH)/%=%)
 $(info $(LOCAL_SRC_FILES))
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/util
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/util $(LOCAL_PATH)/proxy
 APP_CPPFLAGS += -std=c++11
 LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else -Wno-error-implicit-function-declaration -std=c17
 LOCAL_CFLAGS += -DANDROID -pipe -integrated-as -fno-plt -O3 -flto=auto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -funwind-tables -O3 -fvisibility=hidden -fno-semantic-interposition -pthread 
