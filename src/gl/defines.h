6444bddba1fed9cd7306b904d515f4fac7ceb976
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-25 13:08:58 +0800

    Add files via upload

--- src/gl/defines.h
+++ src/gl/defines.h
@@ -1,33 +1,33 @@
 #include <GL/gl.h>
 
 // newly-defined GL functions
-GLboolean glIsList(GLuint list);
-GLuint glGenLists(GLsizei range);
-void glActiveTextureARB(GLenum texture);
-void glArrayElement(GLint i);
-void glBegin(GLenum mode);
-void glCallList(GLuint list);
-void glCallLists(GLsizei n, GLenum type, const GLvoid *lists);
-void glClearDepth(GLdouble depth);
-void glDeleteList(GLuint list);
-void glDeleteLists(GLuint list, GLsizei range);
-void glDrawArrays(GLenum mode, GLint first, GLsizei count);
-void glEnd();
-void glEndList();
-void glFrustum(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near, GLdouble far);
-void glGetDoublev(GLenum pname, GLdouble *params);
-void glIndexf(GLfloat i);
-void glInterleavedArrays(GLenum format, GLsizei stride, const GLvoid *pointer);
-void glListBase(GLuint base);
-void glLockArraysEXT(GLint first, GLsizei count);
-void glNewList(GLuint list, GLenum mode);
-void glOrtho(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near, GLdouble far);
-void glSecondaryColor3f(GLfloat r, GLfloat g, GLfloat b);
-void glTexCoord2f(GLfloat s, GLfloat t);
-void glUnlockArraysEXT();
-void glVertex2f(GLfloat x, GLfloat y);
-void glVertex2i(GLint x, GLint y);
-void glVertex3f(GLfloat x, GLfloat y, GLfloat z);
+GLAPI GLboolean glIsList(GLuint list);
+GLAPI GLuint glGenLists(GLsizei range);
+GLAPI void glActiveTextureARB(GLenum texture);
+GLAPI void glArrayElement(GLint i);
+GLAPI void glBegin(GLenum mode);
+GLAPI void glCallList(GLuint list);
+GLAPI void glCallLists(GLsizei n, GLenum type, const GLvoid *lists);
+GLAPI void glClearDepth(GLdouble depth);
+GLAPI void glDeleteList(GLuint list);
+GLAPI void glDeleteLists(GLuint list, GLsizei range);
+GLAPI void glDrawArrays(GLenum mode, GLint first, GLsizei count);
+GLAPI void glEnd();
+GLAPI void glEndList();
+GLAPI void glFrustum(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near, GLdouble far);
+GLAPI void glGetDoublev(GLenum pname, GLdouble *params);
+GLAPI void glIndexf(GLfloat i);
+GLAPI void glInterleavedArrays(GLenum format, GLsizei stride, const GLvoid *pointer);
+GLAPI void glListBase(GLuint base);
+GLAPI void glLockArraysEXT(GLint first, GLsizei count);
+GLAPI void glNewList(GLuint list, GLenum mode);
+GLAPI void glOrtho(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near, GLdouble far);
+GLAPI void glSecondaryColor3f(GLfloat r, GLfloat g, GLfloat b);
+GLAPI void glTexCoord2f(GLfloat s, GLfloat t);
+GLAPI void glUnlockArraysEXT();
+GLAPI void glVertex2f(GLfloat x, GLfloat y);
+GLAPI void glVertex2i(GLint x, GLint y);
+GLAPI void glVertex3f(GLfloat x, GLfloat y, GLfloat z);
 
 // custom functions
-void glPushCall(void *call);
+GLAPI void glPushCall(void *call);
