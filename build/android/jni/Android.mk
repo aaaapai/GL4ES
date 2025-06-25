bfc2b0e34077a47383eb414b37ed826c20898bf2
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-25 12:33:11 +0800

    Update Android.mk

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -3,7 +3,7 @@ $(info $(LOCAL_PATH))
 include $(CLEAR_VARS)  
 LOCAL_LDLIBS := -ldl -lm -lc
 LOCAL_MODULE    := glshim
-GL_SRC_FILES := $(wildcard $(LOCAL_PATH)/proxy/host/*.c) $(wildcard $(LOCAL_PATH)/proxy/client/src/*.c) $(wildcard $(LOCAL_PATH)/glx/*.c) $(wildcard $(LOCAL_PATH)/gl/*.c) $(wildcard $(LOCAL_PATH)/gl/wrap/*.c) $(wildcard $(LOCAL_PATH)/gl/math/*.c)
+GL_SRC_FILES := $(wildcard $(LOCAL_PATH)/glx/*.c) $(wildcard $(LOCAL_PATH)/gl/*.c) $(wildcard $(LOCAL_PATH)/gl/wrap/*.c) $(wildcard $(LOCAL_PATH)/gl/math/*.c)
 LOCAL_SRC_FILES := $(GL_SRC_FILES:$(LOCAL_PATH)/%=%)
 $(info $(LOCAL_SRC_FILES))
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/util $(LOCAL_PATH)/proxy
