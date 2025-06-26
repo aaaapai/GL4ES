9650b05aa82d01de9695002afafa8c9b4ce07efc
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-06-26 20:06:48 +0800

    Update Android.mk

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -1,15 +1,15 @@
 LOCAL_PATH := $(call my-dir)/../../../src
 $(info $(LOCAL_PATH))
 include $(CLEAR_VARS)  
-LOCAL_LDLIBS := -ldl -lEGL
+LOCAL_LDLIBS := -ldl -lEGL -lm -lc -lGLESv1_CM
 LOCAL_MODULE    := glshim
 GL_SRC_FILES := $(wildcard $(LOCAL_PATH)/glx/*.c) $(wildcard $(LOCAL_PATH)/gl/*.c) $(wildcard $(LOCAL_PATH)/gl/wrap/*.c) $(wildcard $(LOCAL_PATH)/gl/math/*.c)
 LOCAL_SRC_FILES := $(GL_SRC_FILES:$(LOCAL_PATH)/%=%)
 $(info $(LOCAL_SRC_FILES))
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/util $(LOCAL_PATH)/proxy
 APP_CPPFLAGS += -std=c++11
-LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else -Wno-error-implicit-function-declaration -std=c99
+LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else -Wno-error-implicit-function-declaration -std=gnu2x
 LOCAL_CFLAGS += -DANDROID -pipe -integrated-as -fno-plt -O3 -flto=auto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -funwind-tables -O3 -fvisibility=hidden -fno-semantic-interposition -pthread 
 LOCAL_CFLAGS += -DNOX11
-LOCAL_CFLAGS += -DNO_GBM
+LOCAL_CFLAGS += -DNO_GBM -DBOARDWALK_POTATO
 include $(BUILD_SHARED_LIBRARY)
